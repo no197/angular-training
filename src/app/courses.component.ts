146dc3910ce5bb9d66eae54fed276c0eb62d20fc
Yen Mai <yenmai478@gmail.com>
2020-10-27 16:52:09 +0700

    Property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,12 +7,15 @@ import { Component } from '@angular/core';
     <h2>{{number+ ': ' + getTitle()}}</h2>
     <ul>
     <li *ngFor="let course of courses"> {{course}}</li>
+    <img src={{imageUrl}}/>
+    <img [src]="imageUrl" />
     </ul>
   `
 })
 export class CoursesComponent {
   title="List of the courses";
   number = 1;
+  imageUrl = "https://picsum.photos/200/300";
   courses;
   constructor(service: CourseService){
     this.courses = service.getCouses()

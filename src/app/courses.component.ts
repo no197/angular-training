5bfed6e07d7c8bdf4b5421f1792854f30a851421
Yen Mai <yenmai478@gmail.com>
2020-10-27 17:01:18 +0700

    Attribute binding angular
    Angular map DOM --> HTML (so some thing in html not have in DOM) --> Attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,15 +7,22 @@ import { Component } from '@angular/core';
     <h2>{{number+ ': ' + getTitle()}}</h2>
     <ul>
     <li *ngFor="let course of courses"> {{course}}</li>
+    </ul>
     <img src={{imageUrl}}/>
     <img [src]="imageUrl" />
-    </ul>
+
+    <table>
+    <tr>
+    <td [attr.colspan]="colSpan">123</td>
+    </tr>
+    </table>
   `
 })
 export class CoursesComponent {
   title="List of the courses";
   number = 1;
   imageUrl = "https://picsum.photos/200/300";
+  colSpan = 2;
   courses;
   constructor(service: CourseService){
     this.courses = service.getCouses()

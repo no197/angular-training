427db546c545bf2046e65535d980393b0db21ac5
Yen Mai <yenmai478@gmail.com>
2020-10-28 12:34:40 +0700

    Output property
    @Output
    EventEmitter
    call emit --> host component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello-word';
   isSuccess = true;
+
+  buttonChanged() {
+    console.log("Button changed!")
+  }
 }
--- src/app/toggle-button/toggle-button.component.ts
+++ src/app/toggle-button/toggle-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'toggle-button',
@@ -7,9 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ToggleButtonComponent implements OnInit {
   @Input("is-favourite") isSuccess: boolean;
+  @Output() change = new EventEmitter();
 
   onClick() {
     this.isSuccess = !this.isSuccess;
+    this.change.emit();
   }
   constructor() { }
 
